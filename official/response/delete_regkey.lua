791c98b52b0313bb9f75d6359514371e77988588
singlethreaded <chris@infocyte.com>
2020-09-24 18:02:45 -0500

    Add delete_regkey

---
+++ official/response/delete_regkey.lua
@@ -0,0 +1,155 @@
+--[=[ 
+filetype = "Infocyte Extension"
+
+[info]
+name = "Delete Regkey"
+type = "Response"
+description = """Deletes a registry key"""
+author = "Infocyte"
+guid = "5c977e21-0ac1-4328-ab5f-be3ef5f6d06a"
+created = "2020-09-24"
+updated = "2020-09-24"
+
+## GLOBALS ##
+# Global variables accessed within extensions via hunt.global('name')
+
+    [[globals]]
+    name = "deleteregkey_default_key"
+    description = "Registry Key to delete"
+    type = "string"
+    required = true
+
+    [[globals]]
+    name = "deleteregkey_default_keyname"
+    description = "Registry Key Name to delete"
+    type = "string"
+    required = true
+
+    [[globals]]
+    name = "debug"
+    description = "Used to debug the script"
+    type = "boolean"
+    default = false
+
+## ARGUMENTS ##
+# Runtime arguments are accessed within extensions via hunt.arg('name')
+
+    [[args]]
+    name = "key"
+    description = "Keypath to delete"
+    type = "string"
+    required = true
+
+    [[args]]
+    name = "keyname"
+    description = "Key name to delete"
+    type = "string"
+    required = true
+
+]=]
+
+
+--[=[ SECTION 1: Inputs ]=]
+-- hunt.arg(name = <string>, isRequired = <boolean>, [default])
+-- hunt.global(name = <string>, isRequired = <boolean>, [default])
+
+key =   hunt.arg.string("key") or
+        hunt.global.string("deleteregkey_default_key", true)
+keyname =   hunt.arg.string("keyname") or
+            hunt.global.string("deleteregkey_default_keyname", true)
+
+local debug = hunt.global.boolean("debug", false, false) 
+
+--[=[ SECTION 2: Functions ]=]
+
+function string_to_list(str)
+    -- Converts a comma seperated list to a lua list object
+    list = {}
+    for s in string.gmatch(str, '([^,]+)') do
+        table.insert(list, s)
+    end
+    return list
+end
+
+function run_cmd(cmd)    
+    --[=[
+        Runs a command on the default shell and captures output
+        Input:  [string] -- Command
+        Output: [boolean] -- success
+                [string] -- returned message
+    ]=]
+    if debug then hunt.debug("Running command: "..cmd.." 2>&1") end
+    local pipe = io.popen(cmd.." 2>&1", "r")
+    if pipe then
+        local out = pipe:read("*all")
+        pipe:close()
+        if out:find("failed|error|not recognized as an") then
+            hunt.error("[run_cmd] "..out)
+            return false, out
+        else
+            if debug then hunt.debug("[run_cmd] "..out) end
+            return true, out
+        end
+    else 
+        hunt.error("ERROR: No Output from pipe running command "..cmd)
+        return false, "ERROR: No output"
+    end
+end
+
+
+--[=[ SECTION 3: Actions ]=]
+
+host_info = hunt.env.host_info()
+hunt.debug(f"Starting Extention. Hostname: ${host_info:hostname()} [${host_info:domain()}], OS: ${host_info:os()}")
+
+if not hunt.env.is_windows() then 
+    -- Windows only for now
+    hunt.warn(f"Extension is for windows only [${host_info:os()}]")
+    hunt.summary(f"Extension Not Compatible with ${host_info:os()}")
+    return
+end
+
+
+if debug then 
+    -- Debugging, creating test service first
+    hunt.log("Debugging: creating a runkey and deleting it")
+    --reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v Pentestlab /t REG_SZ /d "C:\Users\pentestlab\pentestlab.exe"
+    key = [[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run]]
+    keyname = "TestKey A"
+    value = [['C:\Program Files\test.exe' --hack]]
+    s, out = run_cmd(f"reg add \"${key}\" /v \"${keyname}\" /t REG_SZ /d \"${value}\" /f")
+    os.execute("sleep 3")
+end
+
+key_found = false
+key_deleted = false
+
+-- Find service
+hunt.log(f"Finding and deleting registry key ${keyname} under ${key}")
+--out = hunt.env.run_powershell(f"Get-wmiobject -Query 'Select pathname from win32_service where Name = \"${name}\"' | select -expandproperty pathname") 
+s, out = run_cmd(f"reg query \"${key}\" /v \"${keyname}\"")
+if out:find("The system was unable to find the specified registry key or value.") then 
+    hunt.warn(f"Could not find key ${key} -> '${keyname}': ${out}")       
+elseif out:find(keyname) then
+    value = out:match(f"${keyname}%s.+%s([^\r\n]+)")
+    hunt.log(f"Key found @ ${key} -> '${keyname}'!")
+    key_found = true
+end
+
+-- Delete
+if key_found then
+    s, out = run_cmd(f"reg delete \"${key}\" /v \"${keyname}\" /f")
+    if s and out:find("The operation completed successfully.") then
+        hunt.log(f"${key} -> '${keyname}' deleted!")
+        key_deleted = true
+        hunt.status.good()
+    else
+        hunt.error(f"Could not delete key ${key} -> '${keyname}': ${out}")
+        hunt.status.suspicious()
+    end
+end
+
+-- Print final summary of actions and results
+summary = f"[${key} -> '${keyname}'] Found=${key_found}, Deleted=${key_deleted}"
+hunt.log(summary)
+hunt.summary(summary)
